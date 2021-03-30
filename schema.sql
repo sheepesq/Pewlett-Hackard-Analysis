c6fc5f098fde45dd27f53709ce04d21eae9e0ddf
BrianH <78457490+sheepesq@users.noreply.github.com>
2021-03-29 20:59:30 -0400

    push

--- schema.sql
+++ schema.sql
@@ -37,9 +37,18 @@ CREATE TABLE salaries (
 
 Create Table titles (
 	emp_no INT NOT NULL,
-	title VARCHAR(40) NOT NULL,
+	title VARCHAR NOT NULL,
 	from_date DATE NOT NULL,
   	to_date DATE NOT NULL,
-  FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
-  PRIMARY KEY (emp_no)
+  FOREIGN KEY (emp_no) REFERENCES employees (emp_no)
+ 
+);
+
+CREATE TABLE dept_emp (
+     emp_no INT NOT NULL,
+     dept_no VARCHAR(4) NOT NULL,
+	 from_date DATE NOT NULL,
+  	 to_date DATE NOT NULL,
+  	  FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+ 	 PRIMARY KEY (emp_no, dept_no)
 );
